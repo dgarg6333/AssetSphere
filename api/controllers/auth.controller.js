183f3409b1d40a50444298e93d5a4fd5be317528
deepanshu garg <dgarg6333@gmail.com>
2024-02-20 21:27:46 +0530

    add functionality of the signup page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,24 +3,24 @@ import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
-    const { username, email, password } = req.body;
-  
-    if (  !username ||  !email ||  !password ||  username === '' ||  email === '' ||  password === '') {
-      next(errorHandler(400,'All Fields are required'));
-    }
-    
-    const hassedPassword=bcryptjs.hashSync(password,10);
+  const { username, email, password } = req.body;
 
-    const newUser = new User({
-      username,
-      email,
-      password:hassedPassword,
-    });
-  
-    try {
-      await newUser.save();
-      res.json('Signup successful');
-    } catch (error) {
-      next(error);
-    }
-  };
+  if (  !username ||  !email ||  !password ||  username === '' ||  email === '' ||  password === '') {
+    next(errorHandler(400,'All Fields are required'));
+  }
+
+  const hassedPassword=bcryptjs.hashSync(password,10);
+
+  const newUser = new User({
+    username,
+    email,
+    password:hassedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json('Signup successful');
+  } catch (error) {
+    next(error);
+  }
+};
