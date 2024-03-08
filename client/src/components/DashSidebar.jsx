0ca14fcf51cafc89d30103e8a7a6dae5aa5e10ce
deepanshu garg <dgarg6333@gmail.com>
2024-03-08 14:59:07 +0530

    add dash post section to the dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,17 +1,20 @@
 import { Sidebar } from 'flowbite-react';
 import {
   HiUser,
-  HiArrowSmRight
+  HiArrowSmRight,
+  HiDocumentText,
 } from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { signoutSuccess } from '../redux/user/userSlice';
 
 export default function DashSidebar() {
   const location = useLocation();
   const dispatch=useDispatch();
   const [tab, setTab] = useState('');
+  const {currentUser}=useSelector(state=>state.user);
+
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get('tab');
@@ -44,13 +47,24 @@ export default function DashSidebar() {
             <Sidebar.Item
               active={tab === 'profile'}
               icon={HiUser}
-              label={'User'}
+              label={currentUser.isAdmin?'Admin':'User'}
               labelColor='dark'
               as='div'
             >
               Profile
             </Sidebar.Item>
           </Link>
+          {currentUser.isAdmin && (
+            <Link to='/dashboard?tab=posts'>
+              <Sidebar.Item
+                active={tab === 'posts'}
+                icon={HiDocumentText}
+                as='div'
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
           <Sidebar.Item
             icon={HiArrowSmRight}
             className='cursor-pointer'
