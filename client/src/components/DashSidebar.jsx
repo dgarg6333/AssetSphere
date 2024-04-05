f15aee74d76343bb0ed28f7585e6e2c2e8d2a639
deepanshu garg <dgarg6333@gmail.com>
2024-04-05 23:10:31 +0530

    show users to the admin

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -3,6 +3,7 @@ import {
   HiUser,
   HiArrowSmRight,
   HiDocumentText,
+  HiOutlineUserGroup,
 } from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
@@ -62,6 +63,17 @@ export default function DashSidebar() {
                 as='div'
               >
                 Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+          {currentUser.isAdmin && (
+            <Link to='/dashboard?tab=users'>
+              <Sidebar.Item
+                active={tab === 'posts'}
+                icon={HiOutlineUserGroup}
+                as='div'
+              >
+                Users
               </Sidebar.Item>
             </Link>
           )}
