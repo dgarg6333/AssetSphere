e90099533873e1f6ece0f3f0f1905a2c5645b55c
deepanshu garg <dgarg6333@gmail.com>
2024-05-16 17:19:34 +0530

    resolve error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store , persistor} from './redux/Store.js';
+import { store , persistor} from './redux/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
 import ThemeProvider from './components/ThemeProvider.jsx';
 
