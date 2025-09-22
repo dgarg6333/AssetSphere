b91fd13776befcde28817db5572852ded74a4624
deepanshu garg <dgarg6333@gmail.com>
2025-09-23 03:15:17 +0530

    base url fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import dotenv from "dotenv";
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: process.env.BASE_URL,
         secure: false,
       },
     },
