bbef7cc3f545c54ea6682a4ed41b338d800e0375
deepanshu garg <dgarg6333@gmail.com>
2025-09-23 02:16:23 +0530

    remove oauth

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -3,7 +3,6 @@ import { useState , useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signInStart, signInFailure, signInSuccess } from '../redux/user/userSlice';
-import OAuth from '../components/OAuth';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -115,7 +114,6 @@ export default function SignIn() {
                 'Sign In'
               )}
             </button>
-            <OAuth />
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Do not have an account?</span>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,6 @@
 import { Alert, Label, Spinner, TextInput } from 'flowbite-react'; // Removed Button from import
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import OAuth from '../components/OAuth';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
@@ -118,7 +117,6 @@ export default function SignUp() {
                 'Sign Up'
               )}
             </button>
-            <OAuth />
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an account?</span>
